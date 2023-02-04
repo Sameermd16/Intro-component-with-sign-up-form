9021339ae06a90f2693fb43a1f64907d3184f5f4
sameermd16 <sameerthemc@gmail.com>
2023-02-05 01:01:50 +0530

    JavaScript

--- script.js
+++ script.js
@@ -19,7 +19,8 @@ form.addEventListener("submit", e => {
     }
     if (email === ""){
         addError("email", "Email cannot be empty");
-    }else if(!isValid(email)){
+    }
+    else if(!isValid(email)){
         addError("email", "Email is not valid")
     }
     else{
@@ -36,7 +37,7 @@ form.addEventListener("submit", e => {
 function addError(field, message){
     const formControl = form[field].parentNode;
     formControl.classList.add("error");
-    const small = form["field"].parentNode.querySelector("small");
+    const small = form[field].parentNode.querySelector("small");
     small.innerText = message;
 }
 
@@ -44,3 +45,8 @@ function removeError(field){
     const formControl = form[field].parentNode;
     formControl.classList.remove("error");
 }
+
+function isValid(email){
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(String(email).toLowerCase());
+}  
